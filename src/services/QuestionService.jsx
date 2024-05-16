0ae2a5c606fe5057868ff97e1284d579161258ca
Yusuf Açmacı <72147465+yustyy@users.noreply.github.com>
2024-05-17 02:51:16 +0300

    Update QuestionService.jsx

--- src/services/QuestionService.jsx
+++ src/services/QuestionService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';  
 
 const axiosInstance = axios.create({
-    baseURL: 'https://devrim-backend.onrender.com/api/questions/',
+    baseURL: 'http://backend.yusufacmaci.com/api/questions/',
     withCredentials: true,
   });
 
